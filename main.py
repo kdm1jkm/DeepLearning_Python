c28a712b05c118f3f6ea7d8fdd0dcbd85e17c932
kdm1jkm <kdm1jkmm@gmail.com>
2021-03-17 16:52:52 +0900

    6. 수동 역전파

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import numpy as np
 class Variable:
     def __init__(self, data: np.ndarray) -> None:
         self.data: np.ndarray = data
+        self.grad: np.ndarray = None
 
 
 class Function:
     def __call__(self, input: Variable) -> Variable:
         x = input.data
         y = self.forward(x)
-        return Variable(y)
+        output = Variable(y)
+        self.input = input
+        return output
 
     def forward(self, x: np.ndarray):
+        raise NotImplementedError()
+
+    def backward(self, gy):
         raise NotImplementedError()
 
 
@@ -20,10 +26,16 @@ class Square(Function):
     def forward(self, x: np.ndarray):
         return x ** 2
 
+    def backward(self, gy):
+        return gy * self.input.data * 2
+
 
 class Exp(Function):
     def forward(self, x: np.ndarray):
         return np.exp(x)
+
+    def backward(self, gy):
+        return gy * np.exp(self.input.data)
 
 
 def numerical_diff(f: Function, x: Variable, eps=1e-4) -> np.ndarray:
@@ -34,7 +46,17 @@ def numerical_diff(f: Function, x: Variable, eps=1e-4) -> np.ndarray:
     return (y1.data - y0.data) / (eps * 2)
 
 
-x = Variable(np.array(2.0))
-dy = numerical_diff(Exp(), x)
-print(dy)
-print(Exp()(x).data)
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
