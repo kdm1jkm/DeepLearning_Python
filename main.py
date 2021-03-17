622ddb689470b34dc73731e9a20fd0bc0eca90ee
kdm1jkm <kdm1jkmm@gmail.com>
2021-03-17 15:49:13 +0900

    1. 상자로서의 변수

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    print("Hello, World!")
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data: np.ndarray) -> None:
+        self.data: np.ndarray = data
+
+
+data = np.array(1.0)
+x = Variable(data)
+
+print(x.data)
+
+x.data = np.array(2.0)
+print(x.data)
